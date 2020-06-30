ca8cfda489e68d97c1211508dcf649f7b6e4d097
paritosh verma <30804629+paritosh12@users.noreply.github.com>
2020-07-01 01:39:25 +0530

    Add files via upload

---
+++ CSV_Reader.py
@@ -0,0 +1,39 @@
+######################CSV_Reader##########################
+import tkinter as tk
+from tkinter import filedialog as fd 
+from tkinter import *
+import numpy as np
+import pandas as pd
+from scipy.stats import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class import_csv_data():
+    def __init__(self):
+        print('yes')
+        global v
+        csv_file_path = askopenfilename()
+        print(csv_file_path)
+        v.set(csv_file_path)
+        Dataset= pd.read_csv(csv_file_path)
+        print(Dataset.head)
+
+root = Tk()
+root.configure(background='white')
+
+# Heading
+root.title("by- Paritosh Verma")
+w2 = Label(root, justify=LEFT, text="CSV Reader", fg="white", bg="blue")
+w2.config(font=("Elephant", 10))
+w2.grid(row=1, column=0, columnspan=2, padx=100)
+"""w2 = Label(root, justify=LEFT, text=" ", fg="white", bg="blue")
+w2.config(font=("Aharoni", 10))
+w2.grid(row=2, column=0, columnspan=2, padx=100)"""
+
+#tk.mainloop()
+bt=Button(root, text='Browse Data Set',command=import_csv_data,bg="green",fg="yellow")
+bt.grid(row=4,column=0,padx=100)
+ct=Button(root, text='Close',command=root.destroy,bg="green",fg="yellow")
+ct.grid(row=6,column=0,padx=100)
+
+tk.mainloop()
